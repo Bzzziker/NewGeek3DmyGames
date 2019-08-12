f76d4dcdb0bd101fac60835a4cde891f3f4a782d
Bzzziker <Bzzziker@mail.ru>
2019-08-12 17:06:51 +0500

    12.08 byk

---
+++ NewGeek3DmyGames/Assets/AIControl/Scripts/MyAIContol.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MyAIContol : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator ani;
+
+
+
+    public enum anicontrols
+    {
+        Run,
+        Idle,
+        Attack
+    }
+
+
+
+
+    private void Awake()
+    {
+        agent = this.gameObject.GetComponent<NavMeshAgent>();
+        ani = this.gameObject.GetComponent<Animator>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        ///Код для теста!!!!
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+
+            anicontorl(anicontrols.Run);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anicontorl(anicontrols.Attack);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+
+            anicontorl(anicontrols.Idle);
+        }
+
+
+        ///Конец тест кода
+    }
+
+
+    public void anicontorl(anicontrols str)
+    {
+        switch (str)
+        {
+            case anicontrols.Idle:
+                {
+                    this.ani.SetBool("Idle", true);
+                    this.ani.SetBool("Run", false);
+                    this.ani.SetBool("Attack", false);
+                }
+                break;
+            case anicontrols.Run:
+                {
+                    this.ani.SetBool("Idle", false);
+                    this.ani.SetBool("Run", true);
+                    this.ani.SetBool("Attack", false);
+                }
+                break;
+            case anicontrols.Attack:
+                {
+                    this.ani.SetBool("Idle", false);
+                    this.ani.SetBool("Run", false);
+                    this.ani.SetBool("Attack", true);
+                }
+                break;
+
+        }
+    }
+
+}
---
+++ NewGeek3DmyGames/Assets/PlayerContorl/Scripts/MouseControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{
+
+
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    GameObject test;
+
+
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.mousePosition.x > 2) this.transform.position += transform.right * Time.deltaTime * speed; //пр
+        if (Input.mousePosition.x < Screen.width - 2) this.transform.position -= transform.right * Time.deltaTime * speed; //лев
+        if (Input.mousePosition.y > 2) this.transform.position += transform.forward * Time.deltaTime * speed; //пр
+        if (Input.mousePosition.y < Screen.height - 2) this.transform.position -= transform.forward * Time.deltaTime * speed; //пр
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            test = hit.collider.gameObject;
+
+
+        }
+    }
+}
